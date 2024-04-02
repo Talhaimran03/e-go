96c1206f05f940a0c249a789f5463259b43bce25
AnnaB <annabenettoni@gmail.com>
2024-04-02 20:38:38 +0200

    4aprile_20.38

---
+++ src/pages/settings.js
@@ -0,0 +1,18 @@
+import './css/settings.css'
+import { Link } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Logo from "./components/logo";
+
+
+export default function Settings() {
+    return (
+        <>
+            {/* <div className="pos-logo">
+                <Logo></Logo>
+            </div> */}
+            <div className='pos-navbar'>
+                <Navbar></Navbar>
+            </div>
+        </>
+    );
+}
