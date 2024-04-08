77e99d8439e84177810f609a4ae8a5ef939c8f05
Lorenzo Russo <lorenzo.2000.russo@gmail.com>
2024-04-08 11:36:46 +0200

    icone_inserite

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,8 @@ import './components/components_css/login.css';
 import Logo from "./components/logo.js";
 import { ReactComponent as Background } from "./img/background.svg";
 import { ReactComponent as Icongoogle } from "./img/Google__G__logo.svg";
+import { ReactComponent as Iconuser } from "./img//icona_utente.svg";
+import { ReactComponent as Iconpwd } from "./img/icona_lucchetto.svg";
 
 function Login() {
     return (
@@ -20,7 +22,9 @@ function Login() {
             <div className='flexplace'>
                 <form className='flexform' action="">
                     <input type="text" className='email' placeholder="Email" />
+                    <div className='iconuser'><Iconuser></Iconuser></div>
                     <input type="text" className='password' placeholder="Password" />
+                    <div className='iconpwd'><Iconpwd></Iconpwd></div>
                     <button type='button' className='accedi'>ACCEDI</button>
                     <button type='button' className='signUp'>REGISTRATI</button>
                     <h3 className='pwddimenticata'>password dimenticata?</h3>
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,6 +1,11 @@
 import './components/components_css/signUp.css';
 import { ReactComponent as BkgSignUp } from "./img/BkgSignUp.svg";
 import { ReactComponent as Icongoogle } from "./img/Google__G__logo.svg";
+import { ReactComponent as Arrowreturn } from "./img/Indietro.svg";
+import { ReactComponent as Emailicon } from "./img/email-icon.svg";
+import { ReactComponent as Iconautente } from "./img/icona_utente.svg";
+import { ReactComponent as Calendaricon } from "./img/calendar_month.svg";
+import { ReactComponent as Pswicon } from "./img/icona_lucchetto.svg";
 
 function Sign() {
     return (
@@ -8,6 +13,9 @@ function Sign() {
             <div className='BkgSignUp'>
                 <BkgSignUp></BkgSignUp>
             </div>
+            <div className='arrowreturn'>
+                <Arrowreturn></Arrowreturn>
+                </div>
             <div className='flexcount'>
                 <div className='count1'>1</div>
                 <div className='count2'>2</div>
@@ -15,6 +23,12 @@ function Sign() {
             <div>
                 <h1 className='sign'>REGISTRATI</h1>
             </div>
+                <div className='emailicon'><Emailicon></Emailicon></div>
+                <div className='iconautente'><Iconautente></Iconautente></div>
+                <div className='iconautentesurname'><Iconautente></Iconautente></div>
+                <div className='calendaricon'><Calendaricon></Calendaricon></div>
+                <div className='pswicon'><Pswicon></Pswicon></div>
+                <div className='pswiconrepeat'><Pswicon></Pswicon></div>
             <form className='alignboxsign'>
                 <input type="text" className='boxemail' placeholder='email' />
                 <input type="text" className='boxemail' placeholder='nome' />
@@ -23,7 +37,7 @@ function Sign() {
                 <input type="text" className='boxemail' placeholder='password' />
                 <input type="text" className='boxemail' placeholder='ripeti password' />
             </form>
-            {/* <div className='shapesign'></div> */}
+            <div className='arrowavanti'><Arrowreturn></Arrowreturn></div>
             <div className='buttonalign'>
                 <input type="submit" class="buttonsign" value=""></input>
                 <input type="submit" class="buttongoogle" value=""></input>
