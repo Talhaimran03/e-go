d1b9d99a5b7c42c3d1e14872347f58342dcd12a3
Talhaimran03 <101459540+Talhaimran03@users.noreply.github.com>
2024-04-10 12:19:06 +0200

    navbar

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,36 +1,46 @@
 import './components_css/Navbar.css';
-import Buttons from './Buttons.js';
-import { Link } from 'react-router-dom';
-
+import Button from './Buttons';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as Punti } from "../img/punti.svg";
 import { ReactComponent as Home } from "../img/home.svg";
 import { ReactComponent as Settings } from "../img/impostazioni.svg";
-
-
+import React, { useState, useEffect } from 'react';
 
 function Navbar() {
+    const [activeLink, setActiveLink] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        const pathname = location.pathname;
+        setActiveLink(pathname.split('/')[1] || 'home');
+    }, [location]);
+
+    const handleLinkClick = (linkName) => {
+        setActiveLink(linkName);
+    };
+
     return(
         <div className="navbar">
-            <div className="rowNavbar">
+            <div className="row">
                 <ul className='menu'>
                     <div className="col-4">
                         <li>
-                            <Link to='../points' activeClassName="active">
-                                <Buttons img={ Punti } testo="Punti"/>                    
+                            <Link to='../points' className={activeLink === 'points' ? "link active" : "link"} onClick={() => handleLinkClick('points')}>
+                                <Button img={ Punti } testo={activeLink === 'points' ? "Punti" : " "}/>                    
                             </Link>
                         </li>
                     </div>
                     <div className="col-4">
                         <li>
-                            <Link to='../' activeClassName="">
-                                <Buttons img={ Home } testo="Home"/>   
+                            <Link to='../' className={activeLink === 'home' ? "link active" : "link"} onClick={() => handleLinkClick('home')}>
+                                <Button img={ Home } testo={activeLink === 'home' ? "Home" : " "}/>   
                             </Link>
                         </li>
                     </div>
                     <div className="col-4">
                         <li>
-                            <Link to='../settings' activeClassName="">
-                                <Buttons img={ Settings } testo="Impostazioni"/>   
+                            <Link to='../settings' className={activeLink === 'settings' ? "link active" : "link"} onClick={() => handleLinkClick('settings')}>
+                                <Button img={ Settings } testo={activeLink === 'settings' ? "Impostazioni" : " "}/>   
                             </Link>
                         </li>
                     </div>
