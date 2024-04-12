7d289f27431df7a5f2107830343ecd08a27681e7
akibamfi <akbamfi@gmail.com>
2024-04-12 18:02:24 +0200

    12-04-24 h 18:02

--- src/pages/info.js
+++ src/pages/info.js
@@ -13,9 +13,6 @@ export default function Home() {
             <div className='pos-navbar'>
                 <Navbar></Navbar>
             </div>
-            <div className='bus'>
-                <img src={bus}></img>
-            </div>
             <div className='suDiNoi'>
                 <h2>IL NOSTRO PROGETTO </h2>
             </div>
@@ -27,33 +24,28 @@ export default function Home() {
                     Il nostro obiettivo è quello di creare una web app che possa spingere tuttə ad utilizzare il più possibile i mezzi pubblici presenti sul territorio, usando la consapevolezza come primo approccio. <br />
                     Ma come faremo a renderti più consapevole?<br/>
                     Ti mostreremo quanta CO2 avresti consumato utilizzando un mezzo privato in base alle tratte che farai con un mezzo pubblico. <br/>
-                    <br/>
-                    <br/>
-                    <h3 id='surprise'><strong>Ma le cose belle non sono finite! </strong></h3>
-                    <br/>
-                    <br/>
-                    Come funziona, dunque, la nostra web app? <br/>
-                    Come primo passo dovrai registrarti per creare il tuo profilo personale.<br/>
-                    Una volta eseguito l’accesso avrai a disposizione il grafico della CO2 risparmiata, il calcolo dei punti guadagnati e tutte le impostazioni necessarie. <br/>
-                    <br/>
-                    Il calcolo dei punti viene effettuato in base alle tratte che percorrerai a bordo di un mezzo pubblico, scansionando tramite la nostra web app il QR code presente sul mezzo, in modo da far partire il tracking. <br/>
-                    <br/>
-                    Una volta conclusa la tua corsa dovrai chiudere la tratta sempre tramite la nostra web app, così da poterci permettere di calcolare la distanza che hai percorso e di aggiornare il tuo grafico personale.<br/>
-                    <br/>
-                    Il pezzo forte?<br/>
-                    Dopo ogni utilizzo di un mezzo pubblico otterrai dei punti che potrai poi riscattare, scegliendo tra fantastici premi e sconti. <br/>
-                    <br/>
-                    Di seguito una lista dei nostri partner:<br/>
-                    <br/>
-                    <ul>
-                        <li>Bit Mobility</li>
-                        <li>Amazon</li>
-                        <li>Zalando</li>
-                        <li>JustEat</li>
-                        <li>Steam</li>
-                    </ul>
-
+                    
                     </p>
+
+                <div className='callToAction col-9'> 
+                    <div className='call col-5'>
+                        <p id='fraseCall'> Provala anche tu: </p>
+                          
+                        <p id='puntiCall'>  
+                          <br />
+                            • aiuti l'ambiente. <br />
+                            <br />
+                            • guadagni fantastici premi. <br />
+                            <br />
+                            • farai parte di una comunità consapevole.
+                        </p>
+
+                   </div>
+                   <div className='bottoni col-3'>
+                    <button id='scarica'> Scarica App </button>
+                    <button id='registratiCall'> Registrati </button>
+                   </div>
+                </div>
             </div>
         </>
     );
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Logo from "./components/logo";
 import mask from "./img/mask.png";
+import logo4 from "./img/logo4.png";
 
 
 export default function LandingPage() {
@@ -24,24 +25,35 @@ export default function LandingPage() {
                     <h1 className='frase3'>parte del cambiamento.</h1>
                 </div>
             </div>
-            <footer className='footer'>
-            <div className='col-3 row partner'> 
-                <h2> 10 </h2>
-                <h4> partner</h4>
-            </div>
-            <div className='col-3 row partner2'> 
-                <h2> 10 </h2>
-                <h4> partner</h4>
-            </div>
-            <div className='col-3 row partner3'> 
-                <h2> 10 </h2>
-                <h4> partner</h4>
-            </div>
-            <div className='col-3 row partner4'> 
-                <h2> 10 </h2>
-                <h4> partner</h4>
-            </div>
-
+            <footer>
+                <div className="containerFooter">
+                    <div className="rowFooter">
+                        <div className="col-4">
+                            <img id='logoFooter' src={logo4}></img>
+                            <p id='sottoLogo'> La comunità verso una consapevolezza migliore</p>
+                            
+                        </div>
+                        <div className="col-2 menuFooter">
+                            <h3>Contatti</h3>
+                            <ul>
+                            <li> <p id='mail'>Email: info@tuazienda.com</p></li>
+                            <li> <p id='telefono'> Telefono: 123-456-7890</p></li>
+                            <li><a href="/contatti">Contatti</a></li>
+                            <li><a href="/faq">FAQ</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-2 menuFooter">
+                            <h3>Link Utili</h3>
+                            <ul>
+                            <li><a href="/">Contatti</a></li>
+                            <li><a href="/chi-siamo">Chi siamo</a></li>
+                            <li><a href="/contatti">Contatti</a></li>
+                            <li><a href="/faq">FAQ</a></li>
+                            </ul>
+                        </div>
+                      
+                    </div>
+                </div>
             </footer>
         </div>
     );
