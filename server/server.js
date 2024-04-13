17a09fc217b31d666a7ba778591f6ca598c145ab
Talhaimran03 <101459540+Talhaimran03@users.noreply.github.com>
2024-04-13 20:59:37 +0200

    https for webcam

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// server/server.js
+
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 4000;
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server running at https://localhost:${port}/`);
+});
