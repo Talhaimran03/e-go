74ff9db4c3afd5d3a42d850b0f9321dfee95c5bb
Lorenzo Russo <lorenzo.2000.russo@gmail.com>
2024-04-16 19:02:34 +0200

    Update WebConfig.java

--- backend/api-ego/ego-module/src/main/java/ego/util/WebConfig.java
+++ backend/api-ego/ego-module/src/main/java/ego/util/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/ego/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
