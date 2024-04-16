dedc7e4897bef8ed54b32d258ff2040e13a30036
cl0ud-ia <150252538+cl0ud-ia@users.noreply.github.com>
2024-04-16 22:35:31 +0200

    style error404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,71 @@
 import {Link} from "react-router-dom"
 import './css/NotFound.css'
+import Ego from './img/ego.svg'
 
 const NotFound = () => {
+
+    return (
+        <div class="banner__container">
+            <div class="banner__track track1">
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+            </div>
+            <div class="banner__track track2">
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+            </div>
+            <div class="banner__track track3">
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+            </div>
+
+
+            <div class="banner__trackUno track5">
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+            </div>
+            <div class="banner__trackUno track4">
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+                <p class="banner__text404">404</p>
+            </div>
+            <div class="banner__trackUno track6">
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+                <p class="banner__text404">Errore</p>
+            </div>
+
+
+            <div className="overlayer">
+                <div className="egoLogo">
+                    <img src={Ego} alt="Logo"></img>                
+                </div>
+                <p className="funny"> OOPS... </p>
+                <p className="informative"> Impossibile trovare la pagina che stai cercando </p>
+                <Link to="/">
+                    <div className="turnBack">
+                        <p>Torna alla Home</p>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default NotFound;    
