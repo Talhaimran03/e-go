1c42a35859eef56e47194798b2dc4881b35a77eb
AnnaB <annabenettoni@gmail.com>
2024-04-16 17:31:41 +0200

    anna achi e claudia aposto

--- src/pages/info.js
+++ src/pages/info.js
@@ -8,7 +8,7 @@ export default function Home() {
 
             <svg
                     version="1.1"
-                    id="bgGrande"
+                    id="bgGrande-info"
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                     x="0px"
@@ -39,7 +39,7 @@ export default function Home() {
                 />
             </svg>
             <div className='bgPiccoloDiv'>
-                <svg id='bgPiccolo' width="397" height="477" viewBox="0 0 397 477" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg id='bgPiccolo-info' width="397" height="477" viewBox="0 0 397 477" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 25.5C0 11.6929 11.1929 0.5 25 0.5H372C385.807 0.5 397 11.6929 397 25.5V451.5C397 465.307 385.807 476.5 372 476.5H232.551C218.744 476.5 207.551 465.307 207.551 451.5V398.5C207.551 384.693 196.358 373.5 182.551 373.5H158.114C144.306 373.5 133.114 362.307 133.114 348.5V318.308C133.114 309.301 125.812 302 116.806 302V302C107.8 302 100.498 294.699 100.498 285.692V264C100.498 250.193 89.3055 239 75.4984 239H25C11.1929 239 0 227.807 0 214V25.5Z" fill="url(#paint0_linear_55_343)"/>
                     <defs>
                     <linearGradient id="paint0_linear_55_343" x1="198.5" y1="-3" x2="198.5" y2="473" gradientUnits="userSpaceOnUse">
@@ -50,64 +50,64 @@ export default function Home() {
                 </svg>
             </div>    
 
-            <div className='logoResponsive'>
-                <img id='logoResp' src={EGOGOMMA}></img>
+            <div className='logoResponsive-info'>
+                <img id='logoResp-info' src={EGOGOMMA}></img>
             </div>
                 
 
-            <nav className='navbarDef'>
-                <div className='navbarSx'>
+            <nav className='navbarDef-info'>
+                <div className='navbarSx-info'>
                     <a href='#'><h4> Home </h4></a>
                     <a href='#'><h4> Impostazioni </h4></a>
                     <a href='#'><h4> Punti </h4></a>
                 </div>
-                <div className='imgEgo'>
-                 <img className='imgEgo' src={EGOGOMMA}></img>
+                <div className='imgEgo-info'>
+                 <img className='imgEgo-info' src={EGOGOMMA}></img>
                 </div>
-                <div className='navbarDx'>
+                <div className='navbarDx-info'>
                     <a href='#'> <h4> Contattaci </h4></a>
                 </div>
             </nav>
-            <div className='contentMain'>
-                <div className='contentSx'>
-                    <div className='question'>
-                        <h3 id='contentQuestion'> Cosa aspetti? </h3>
+            <div className='contentMain-info'>
+                <div className='contentSx-info'>
+                    <div className='question-info'>
+                        <h3 id='contentQuestion-info'> Cosa aspetti? </h3>
                     </div>
-                    <div className='buttons'>
-                        <button id='btnAccedi'> Accedi </button>                        
-                        <button id='btnRegistrati'> Registrati </button>                        
+                    <div className='buttons-info'>
+                        <button id='btnAccedi-info'> Accedi </button>                        
+                        <button id='btnRegistrati-info'> Registrati </button>                        
                     </div>
                 </div>
-                <div className='imgContent'>
-                    <img id='iphone' src={iphoneDef}></img>
+                <div className='imgContent-info'>
+                    <img id='iphone-info' src={iphoneDef}></img>
                 </div>
-                <div className='contentUs'>
-                    <p id='aboutUs'>
+                <div className='contentUs-info'>
+                    <p id='aboutUs-info'>
                         Il nostro obiettivo è quello di creare una web app che possa spingere tuttə ad utilizzare il più possibile i mezzi pubblici presenti sul territorio, 
                         usando la consapevolezza come primo approccio. 
                     </p>
                 </div>
-                <div className='endContentResp'>
-                <div className='firstResp'>
-                    <h1 id='firstpResp'> Lavoriamo per <br/> spronarti a prendere </h1>
+                <div className='endContentResp-info'>
+                <div className='firstResp-info'>
+                    <h1 id='firstpResp-info'> Lavoriamo per <br/> spronarti a prendere </h1>
                 </div>
-                <div className='secondResp'>
-                    <h1 id='secondpResp'> consapevolmente</h1>
+                <div className='secondResp-info'>
+                    <h1 id='secondpResp-info'> consapevolmente</h1>
                 </div>
-                <div className='thirdResp'>
-                    <h1 id='thirdpResp'>parte del cambiamento. </h1>
+                <div className='thirdResp-info'>
+                    <h1 id='thirdpResp-info'>parte del cambiamento. </h1>
                 </div>
             </div>
             </div>
-            <div className='endContent'>
-                <div className='first'>
-                    <h1 id='firstp'> Lavoriamo per <br/> spronarti a prendere </h1>
+            <div className='endContent-info'>
+                <div className='first-info'>
+                    <h1 id='firstp-info'> Lavoriamo per <br/> spronarti a prendere </h1>
                 </div>
-                <div className='second'>
-                    <h1 id='secondp'> consapevolmente</h1>
+                <div className='second-info'>
+                    <h1 id='secondp-info'> consapevolmente</h1>
                 </div>
-                <div className='third'>
-                    <h1 id='thirpd'>parte del cambiamento. </h1>
+                <div className='third-info'>
+                    <h1 id='thirdp-info'>parte del cambiamento. </h1>
                 </div>
             </div>
         </>
