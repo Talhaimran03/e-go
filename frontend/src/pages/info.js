4abfd5fd465c64b9a345cd2aa4ab61c60fa6c23b
AnnaB <annabenettoni@gmail.com>
2024-04-17 16:37:46 +0200

    responsive e info appost fratm

--- frontend/src/pages/info.js
+++ frontend/src/pages/info.js
@@ -9,35 +9,35 @@ export default function Home() {
         <>
 
             <svg
-                    version="1.1"
-                    id="bgGrande-info"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 1272 680"
-                    xmlSpace="preserve"
-                >
-            
+                version="1.1"
+                id="bgGrande-info"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 1272 680"
+                xmlSpace="preserve"
+            >
+
                 <style type="text/css">
-                {`.st0{fill:url(#Blocco_00000132060931026753572040000009978651570647462568_);}`}
+                    {`.st0{fill:url(#Blocco_00000132060931026753572040000009978651570647462568_);}`}
                 </style>
                 <linearGradient
-                id="Blocco_00000158011681888672957110000005131347568930586255_"
-                gradientUnits="userSpaceOnUse"
-                x1="628.95"
-                y1="119.6"
-                x2="628.95"
-                y2="715.6"
-                gradientTransform="matrix(1 0 0 1 0 -86)"
+                    id="Blocco_00000158011681888672957110000005131347568930586255_"
+                    gradientUnits="userSpaceOnUse"
+                    x1="628.95"
+                    y1="119.6"
+                    x2="628.95"
+                    y2="715.6"
+                    gradientTransform="matrix(1 0 0 1 0 -86)"
                 >
-                <stop offset="0" style={{ stopColor: '#4AA18B' }} />
-                <stop offset="1" style={{ stopColor: '#1B3B33' }} />
+                    <stop offset="0" style={{ stopColor: '#4AA18B' }} />
+                    <stop offset="1" style={{ stopColor: '#1B3B33' }} />
                 </linearGradient>
                 <path
-                id="Blocco"
-                style={{ fill: 'url(#Blocco_00000158011681888672957110000005131347568930586255_)' }}
-                d="M45.5,55.5c0-12.1,10.3-21.9,22.9-21.9h1121.1c12.7,0,22.9,9.8,22.9,21.9v552.2c0,12.1-10.3,21.9-22.9,21.9h-511c-12.7,0-22.9-9.8-22.9-21.9v-33.3c0-12.1-10.3-21.9-22.9-21.9h-173c-12.7,0-22.9-9.8-22.9-21.9v-33.7c0-12.1-10.3-21.9-22.9-21.9h-50c-12.7,0-22.9-9.8-22.9-21.9v-29.4c0-12.1-10.3-21.9-22.9-21.9H68.4c-12.7,0-22.9-9.8-22.9-21.9V55.5z"
+                    id="Blocco"
+                    style={{ fill: 'url(#Blocco_00000158011681888672957110000005131347568930586255_)' }}
+                    d="M45.5,55.5c0-12.1,10.3-21.9,22.9-21.9h1121.1c12.7,0,22.9,9.8,22.9,21.9v552.2c0,12.1-10.3,21.9-22.9,21.9h-511c-12.7,0-22.9-9.8-22.9-21.9v-33.3c0-12.1-10.3-21.9-22.9-21.9h-173c-12.7,0-22.9-9.8-22.9-21.9v-33.7c0-12.1-10.3-21.9-22.9-21.9h-50c-12.7,0-22.9-9.8-22.9-21.9v-29.4c0-12.1-10.3-21.9-22.9-21.9H68.4c-12.7,0-22.9-9.8-22.9-21.9V55.5z"
                 />
             </svg>
             <div className='bgPiccoloDiv'>
@@ -45,18 +45,18 @@ export default function Home() {
                     <path d="M0 25.5C0 11.6929 11.1929 0.5 25 0.5H372C385.807 0.5 397 11.6929 397 25.5V451.5C397 465.307 385.807 476.5 372 476.5H232.551C218.744 476.5 207.551 465.307 207.551 451.5V398.5C207.551 384.693 196.358 373.5 182.551 373.5H158.114C144.306 373.5 133.114 362.307 133.114 348.5V318.308C133.114 309.301 125.812 302 116.806 302V302C107.8 302 100.498 294.699 100.498 285.692V264C100.498 250.193 89.3055 239 75.4984 239H25C11.1929 239 0 227.807 0 214V25.5Z" fill="#4AA18B"
                     />
                     <defs>
-                    <linearGradient id="paint0_linear_55_343" x1="198.5" y1="-3" x2="198.5" y2="473" gradientUnits="userSpaceOnUse">
-                    <stop stopColor="#4AA18B"/>
-                    <stop offset="1" stoColor="#1B3B33"/>
-                    </linearGradient>
+                        <linearGradient id="paint0_linear_55_343" x1="198.5" y1="-3" x2="198.5" y2="473" gradientUnits="userSpaceOnUse">
+                            <stop stopColor="#4AA18B" />
+                            <stop offset="1" stoColor="#1B3B33" />
+                        </linearGradient>
                     </defs>
                 </svg>
-            </div>    
+            </div>
 
             <div className='logoResponsive-info'>
                 <img id='logoResp-info' src={EGOGOMMA}></img>
             </div>
-                
+
 
             <nav className='navbarDef-info'>
                 <div className='navbarSx-info'>
@@ -65,12 +65,13 @@ export default function Home() {
                     <Link to='../points'><h4> Punti </h4></Link>
                 </div>
                 <div className='imgEgo-info'>
-                 <img className='imgEgo-info' src={EGOGOMMA}></img>
+                    <img className='imgEgo-info' src={EGOGOMMA}></img>
                 </div>
                 <div className='navbarDx-info'>
                     <a href='#'> <h4> Contattaci </h4></a>
                 </div>
             </nav>
+            
             <div className='contentMain-info'>
                 <div className='contentSx-info'>
                     <div className='question-info'>
@@ -78,10 +79,10 @@ export default function Home() {
                     </div>
                     <div className='buttons-info'>
                         <Link to='../login' id="btnSize-info">
-                            Accedi                 
+                            Accedi
                         </Link>
                         <Link to='../signUp' id="btnSize-info">
-                            Registrati                   
+                            Registrati
                         </Link>
                     </div>
                 </div>
@@ -90,25 +91,25 @@ export default function Home() {
                 </div>
                 <div className='contentUs-info'>
                     <p id='aboutUs-info'>
-                        Il nostro obiettivo è quello di creare una web app che possa spingere tuttə ad utilizzare il più possibile i mezzi pubblici presenti sul territorio, 
-                        usando la consapevolezza come primo approccio. 
+                        Il nostro obiettivo è quello di creare una web app che possa spingere tuttə ad utilizzare il più possibile i mezzi pubblici presenti sul territorio,
+                        usando la consapevolezza come primo approccio.
                     </p>
                 </div>
                 <div className='endContentResp-info'>
-                <div className='firstResp-info'>
-                    <h1 id='firstpResp-info'> Lavoriamo per <br/> spronarti a prendere </h1>
+                    <div className='firstResp-info'>
+                        <h1 id='firstpResp-info'> Lavoriamo per <br /> spronarti a prendere </h1>
+                    </div>
+                    <div className='secondResp-info'>
+                        <h1 id='secondpResp-info'> consapevolmente</h1>
+                    </div>
+                    <div className='thirdResp-info'>
+                        <h1 id='thirdpResp-info'>parte del cambiamento. </h1>
+                    </div>
                 </div>
-                <div className='secondResp-info'>
-                    <h1 id='secondpResp-info'> consapevolmente</h1>
-                </div>
-                <div className='thirdResp-info'>
-                    <h1 id='thirdpResp-info'>parte del cambiamento. </h1>
-                </div>
-            </div>
             </div>
             <div className='endContent-info'>
                 <div className='first-info'>
-                    <h1 id='firstp-info'> Lavoriamo per <br/> spronarti a prendere </h1>
+                    <h1 id='firstp-info'> Lavoriamo per <br /> spronarti a prendere </h1>
                 </div>
                 <div className='second-info'>
                     <h1 id='secondp-info'> consapevolmente</h1>
--- frontend/src/pages/ranking.js
+++ frontend/src/pages/ranking.js
@@ -31,18 +31,18 @@ export default function Ranking() {
             </div>
 
             <div className="container-ranking">
-                <RankingSection number='1' icon={ User } text='Talha Imran' points='1009p.'/>
-                <RankingSection number='2' icon={ User } text='Marco Bosco' points='976p.'/>
-                <RankingSection number='3' icon={ User } text='Anna Benettoni' points='943p.'/>
-                <RankingSection number='4' icon={ User } text='Achille Bamfi' points='891p.'/>
-                <RankingSection number='5' icon={ User } text='Tu' points='888p.' />
-                <RankingSection number='6' icon={ User } text='Lorenzo Russo' points='839p.'/>
-                <RankingSection number='7' icon={ User } text='Sofia Spiniella' points='821p.'/>
-                <RankingSection number='8' icon={ User } text='Claudia Garofolin' points='818p.'/>
-                <RankingSection number='9' icon={ User } text='Tommaso Cocco' points='715p.'/>
-                <RankingSection number='10' icon={ User } text='Angelica Ballarotto' points='633p.'/>
-                <RankingSection number='11' icon={ User } text='Yasmine Giuliani' points='614p.'/>
-                <RankingSection number='12' icon={ User } text='Diego Milli' points='528p.'/>
+                <RankingSection number='1' icon={ User } text='Talha Imran' points='710p.'/>
+                <RankingSection number='2' icon={ User } text='Marco Bosco' points='590p.'/>
+                <RankingSection number='3' icon={ User } text='Anna Benettoni' points='510p.'/>
+                <RankingSection number='4' icon={ User } text='Achille Bamfi' points='450p.'/>
+                <RankingSection number='5' icon={ User } text='Tu' points='410p.' />
+                <RankingSection number='6' icon={ User } text='Lorenzo Russo' points='390p.'/>
+                <RankingSection number='7' icon={ User } text='Sofia Spiniella' points='320p.'/>
+                <RankingSection number='8' icon={ User } text='Claudia Garofolin' points='210p.'/>
+                <RankingSection number='9' icon={ User } text='Tommaso Cocco' points='200p.'/>
+                <RankingSection number='10' icon={ User } text='Angelica Ballarotto' points='180p.'/>
+                <RankingSection number='11' icon={ User } text='Yasmine Giuliani' points='120p.'/>
+                <RankingSection number='12' icon={ User } text='Diego Milli' points='50p.'/>
             </div>
             <Link to="/Qr">
                 <div className="qrPosition-ranking">
